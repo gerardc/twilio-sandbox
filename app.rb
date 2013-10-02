10adc7cdaa0a337effa0ba3a8e43082644bffcc8
Ciarán Archer <carcher@zendesk.com>
2013-10-02 18:05:52 +0100

    WIP - putting in hold

--- app.rb
+++ app.rb
@@ -32,6 +32,7 @@ class App < Sinatra::Base
     set :account_sid, (ENV["ACCOUNT_SID"] || twilio_config["account_sid"])
     set :auth_token, (ENV["AUTH_TOKEN"] || twilio_config["auth_token"])
     set :queue_name, "queue_name"
+    set :hold_queue_name, "hold_queue_name"
   end
 
   before do
@@ -73,6 +74,7 @@ class App < Sinatra::Base
     client = ::Twilio::REST::Client.new settings.account_sid, settings.auth_token
     call = client.account.calls.get(CurrentCall.sid)
     url = url("#{params[:splat].first}")
+    p params[:splat]
     call.update(:url => url, :method => "POST")
   end
 
@@ -105,6 +107,84 @@ class App < Sinatra::Base
   post '/enqueue' do
     response = Twilio::TwiML::Response.new do |r|
       r.Enqueue(settings.queue_name, "waitUrl" => url("/queue_wait"))
+    end
+    response.text
+  end
+
+  ### CALL HOLD
+
+  post '/queue_hold' do
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say "You are now on hold!"
+    end
+    response.text
+  end
+
+  post '/hold/:music' do
+    response = Twilio::TwiML::Response.new do |r|
+      if params[:music].to_i == 0
+        r.Play "https://api.twilio.com/cowbell.mp3", :loop => 100
+      else
+        r.Play "https://api.twilio.com/cowbell.mp3", :loop => 100
+      end
+    end
+    response.text
+  end
+
+  # called directly
+  post '/put_all_on_hold' do
+    client = ::Twilio::REST::Client.new settings.account_sid, settings.auth_token
+    client.account.calls.list({:status => "in-progress"}).each_with_index do |call, i|
+      call = client.account.calls.get(call.sid)
+      call.update(:url => url("hold/#{i}"), :method => "POST")
+    end
+  end
+
+
+  ## CONFERENCE STUFF
+
+  # Called directly
+  post '/start_conference' do
+    client = ::Twilio::REST::Client.new settings.account_sid, settings.auth_token
+
+    call = client.account.calls.get(CurrentCall.sid)
+    call.update(:url => url("put_in_conference"), :method => "POST")
+
+  end
+
+  post '/put_in_conference' do
+    response = Twilio::TwiML::Response.new do |r|
+      r.Dial do |d|
+        d.Conference "zendesk_conf", :waitUrl => 'http://twimlets.com/holdmusic?Bucket=com.twilio.music.electronica'
+      end
+    end
+    response.text
+  end
+
+  post '/noop' do
+  end
+
+  # Called directly (1)
+  post '/put_client_in_conference/:client' do
+    client = ::Twilio::REST::Client.new settings.account_sid, settings.auth_token
+    call = client.account.calls.create(
+      :from => '+13257161992',
+      :to => "client:#{params[:client]}", #?
+      :url => url('noop')
+    )
+
+    puts "sleeping..."
+    sleep(5)
+    puts "waking!"
+
+    call.update(:url => url("put_agent_in_conference"), :method => "POST")
+  end
+
+  post '/put_agent_in_conference' do
+    response = Twilio::TwiML::Response.new do |r|
+      r.Dial do |d|
+        d.Conference "zendesk_conf", :waitUrl => 'http://twimlets.com/holdmusic?Bucket=com.twilio.music.electronica'
+      end
     end
     response.text
   end
